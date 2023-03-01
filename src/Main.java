b73949f9e076461023218fa82f16858f9c24cbb5
Irynka88 <kovalchuk22.irunka@gmail.com>
2023-03-01 10:59:12 +0200

    solved the exercise

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // task1
+        int num1 = 11;
+        int num2 = 5;
+        double num3 = (double) num1/num2;
+        System.out.println(num3);
+        int num4 = num1%num2;
+        System.out.println(num4);
+        // tesk2
+        int a = 23;
+        System.out.println((a/10)+(a%10));
+        // tesk3
+        double a1 = 23.3 ;
+        System.out.println((int)a1);
+    }
+}
